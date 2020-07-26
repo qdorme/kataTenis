e097cd3ccd3dc6768f77d8a8614a720dec630a42
qdorme <qdorme@gmail.com>
2020-07-26 17:11:52 +0200

    Add a Demo class

---
+++ src/main/java/kata/tennis/Demo.java
@@ -0,0 +1,41 @@
+package kata.tennis;
+
+
+import java.util.stream.IntStream;
+
+public class Demo {
+    public static void main(String[] args) {
+        Game game = new Game(new Screen(System.out));
+        playerWonPoints(game,1,1);
+        game.displayPlayersScores();
+        game.displayPlayersSetScores();
+
+        System.out.println("");
+        playerWonSet(game,1,4);
+        playerWonSet(game,2,4);
+        game.displayPlayersScores();
+        game.displayPlayersSetScores();
+
+        System.out.println("");
+        playerWonSet(game, 2,2);
+        game.displayPlayersScores();
+        game.displayPlayersSetScores();
+
+        System.out.println("");
+        game = new Game(new Screen(System.out));
+        playerWonSet(game,1,5);
+        playerWonSet(game,2,6);
+        playerWonSet(game,1,1);
+        playerWonSet(game,2,2);
+        game.displayPlayersScores();
+        game.displayPlayersSetScores();
+    }
+
+    static void playerWonPoints(Game game, int player, int times){
+        IntStream.range(0,times).forEach(time -> game.pointWonByPlayer(player));
+    }
+
+    static void playerWonSet(Game game, int player, int times){
+        IntStream.range(0,times).forEach(time -> playerWonPoints(game,player,4));
+    }
+}
